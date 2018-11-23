0728f3549160d8872effdc8477b39a892dfa8889
Lorincz Peter <lorinczpeter94@yahoo.com>
2018-11-23 20:11:36 +0200

    Clickable cells, one circle shows up

--- app/src/main/java/com/example/lorinczpeter94/chainreaction2/gameActivity/LargeGameActivity.kt
+++ app/src/main/java/com/example/lorinczpeter94/chainreaction2/gameActivity/LargeGameActivity.kt
@@ -1,4 +1,4 @@
-package com.example.lorinczpeter94.chainreaction2.game_activity
+package com.example.lorinczpeter94.chainreaction2.gameActivity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -6,11 +6,11 @@ import android.widget.Toast
 import com.example.lorinczpeter94.chainreaction2.R
 import com.example.lorinczpeter94.chainreaction2.welcome_activity.PLAYERNUM
 
-class MediumGameActivity : AppCompatActivity() {
+class LargeGameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_medium_game)
+        setContentView(R.layout.activity_large_game)
         val playerNumber = intent.getIntExtra(PLAYERNUM, 2)
         Toast.makeText(this, "Player number selected: $playerNumber", Toast.LENGTH_LONG).show()
     }
---
+++ app/src/main/java/com/example/lorinczpeter94/chainreaction2/gameActivity/view/IGameView.kt
@@ -0,0 +1,9 @@
+package com.example.lorinczpeter94.chainreaction2.gameActivity.view
+
+import android.widget.ImageView
+
+interface IGameView{
+    fun setOnecircle(imageView: ImageView)
+    fun setTwoCircles(imageView: ImageView)
+    fun setThreeCircles(imageView: ImageView)
+}
